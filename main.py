443d4092c23c220f0193e888824695dc7b262201
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2024-11-06 23:53:26 -0300

    Tested for coordinate

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import asyncio
 
 from google.maps import solar_v1
+from google.type import latlng_pb2
 
 
 async def main():
     client = solar_v1.SolarAsyncClient.from_service_account_file("credentials.json")
 
-    request = solar_v1.FindClosestBuildingInsightsRequest()
+    location = latlng_pb2.LatLng(latitude=-22.8447, longitude=-43.2327)
+
+    request = solar_v1.FindClosestBuildingInsightsRequest(
+        location=location,
+        required_quality=solar_v1.ImageryQuality.HIGH,
+        exact_quality_required=False,
+    )
     response = await client.find_closest_building_insights(request=request)
 
-    print(response)
+    with open("output.json", "w") as f:
+        f.write(str(response))
 
 
 if __name__ == "__main__":
